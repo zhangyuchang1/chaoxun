8001d66be5ed5b5931afdb1cdec15f9e64638c1f
zyc <zyc@zycdeiMac.local>
2016-03-02 17:28:39 +0800

    尝试 颤抖动画

--- ChaoXun/ChaoXun/AddressItemView.m
+++ ChaoXun/ChaoXun/AddressItemView.m
@@ -20,6 +20,13 @@
     label.text = @"佳佳";
     label.tag = Label_TAG;
     [self addSubview:label];
+    
+    
+    UIButton *b = [[UIButton alloc] initWithFrame:label.frame];
+    [b addTarget:self action:@selector(chancle) forControlEvents:UIControlEventTouchUpOutside];
+    [b addTarget:self action:@selector(touch) forControlEvents:UIControlEventTouchDown];
+ 
+    [self addSubview:b];
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
@@ -37,6 +44,7 @@
     for (UIView *view in self.subviews) {
         [view.layer removeAllAnimations];
     }
+    [self stopAnimate];
 
 }
 -(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
@@ -49,10 +57,10 @@
 
 -(void)shakeAnimate
 {
-    //label颤抖 bu
+    //label颤抖  不好停止
 //    UILabel *label = [self viewWithTag:Label_TAG];
 //    CGAffineTransform  transformIdentifer =  self.transform;
-//    [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+//    [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveLinear|UIViewAnimationOptionRepeat animations:^{
 //        CGAffineTransform  rotate = CGAffineTransformRotate(transformIdentifer, M_PI/6);
 //        self.transform = rotate;
 //    } completion:^(BOOL finished) {
@@ -65,8 +73,7 @@
 //            [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
 //                self.transform = transformIdentifer;
 //            } completion:^(BOOL finished) {
-//                [self shakeAnimate];
-//
+////                [self shakeAnimate];
 //            }];
 //            
 //
@@ -79,24 +86,45 @@
     shakAnimate1.autoreverses = YES;
 //    shakAnimate1.repeatDuration = HUGE_VALF;
     shakAnimate1.duration = 1;
-    shakAnimate1.fromValue = [NSNumber numberWithFloat:0];
+    shakAnimate1.fromValue = [NSNumber numberWithFloat:-M_PI/6];
     shakAnimate1.toValue=[NSNumber numberWithFloat:M_PI/6];
 //    [self.layer addAnimation:shakAnimate1 forKey:@"appDeleteShakeAnimation"];
     
     CABasicAnimation *shakAnimate2 = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
-    shakAnimate1.autoreverses = YES;
+    shakAnimate2.autoreverses = YES;
 //    shakAnimate1.repeatDuration = HUGE_VALF;
-    shakAnimate1.duration = 2;
-    shakAnimate1.fromValue = [NSNumber numberWithFloat:M_PI/6];
-    shakAnimate1.toValue=[NSNumber numberWithFloat:-M_PI/6];
-//    [self.layer addAnimation:shakAnimate1 forKey:@"appDeleteShakeAnimation"];
+    shakAnimate2.duration = 1;
+    shakAnimate2.fromValue = [NSNumber numberWithFloat:M_PI/6];
+    shakAnimate2.toValue=[NSNumber numberWithFloat:-M_PI/6];
+//    [self.layer addAnimation:shakAnimate2 forKey:@"appDeleteShakeAnimation"];
     
     CAAnimationGroup *shakAnimate = [CAAnimationGroup animation];
     shakAnimate.animations = @[shakAnimate1,shakAnimate2];
-    shakAnimate.repeatCount = 3;
-    [self.layer addAnimation:shakAnimate forKey:@"shakAnimate"];
+//    shakAnimate1.repeatDuration = HUGE_VALF;
+    shakAnimate.autoreverses = YES;
+    shakAnimate.repeatCount = 5;
+    shakAnimate.duration = 2;
+    [self.layer addAnimation:shakAnimate forKey:@"appDeleteShakeAnimation"];
 }
 
 
+-(void)stopAnimate
+{
+    UILabel *label = [self viewWithTag:Label_TAG];
 
+    [UIView animateWithDuration:0.1 animations:^{
+        CGAffineTransform  transformIdentifer =  self.transform;
+        CGAffineTransform  rotate = CGAffineTransformRotate(transformIdentifer, 0.1);
+        self.transform = rotate;
+    }];
+}
+
+-(void)touch
+{
+    [self shakeAnimate];
+}
+-(void)chancle
+{
+    [self stopAnimate];
+}
 @end
